ca58dedc672cc6b524b97d7b3699b9d01c3d0833
ddorn <diego.dorn@free.fr>
2017-10-22 10:41:19 +1100

    added man in the installed lib

--- update.cmd
+++ update.cmd
@@ -1,2 +1,7 @@
+:: This script updates the two copies of man.py based on the one in libtemplate
 @echo off
+:: The one to manage my package
 cp man/libtemplate/man.py man.py
+:: The one to install with the module
+cp man/libtemplate/man.py man/man.py
+echo Done!
