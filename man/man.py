ea404c621500390b16d9017d9a53cd07fce31bed
ddorn <diego.dorn@free.fr>
2017-10-23 22:11:50 +1100

    list prints for tests

--- man/man.py
+++ man/man.py
@@ -451,7 +451,7 @@ class ManCLi(AliasCLI):
         # run('pip uninstall %s --yes' % config.libname)
 
         # make sure it passes the tests
-        if run('pytest test') != 0:
+        if run('pytest test --quiet') != 0:
             click.secho("The tests doesn't pass.", fg='red')
             revert_version()
             return
